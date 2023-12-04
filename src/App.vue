4037e317447c7ab9134622ae90416ad673bbaf3a
dreamer-was-taken <emaore0707@gmail.com>
2023-12-04 12:19:57 +0100

    this is my finaly project for view
    it going to be similar to the anime buzz app but its mostly for gamin news aticles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
 
 export default {
+  components:{
+Nav,
+  },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+margin: 2rem 2.5rem;
 }
 </style>
